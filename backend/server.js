e6b46fa0d664d600ebd09c3e1b5b99cfbe5c2f4f
Paxden <paxdenco@gmail.com>
2025-09-26 10:03:18 +0100

    manifest

--- backend/server.js
+++ backend/server.js
@@ -16,18 +16,7 @@ const app = express();
 const allowedOrigins = ["http://localhost:5173", "https://pax-vms.vercel.app"];
 
 app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like Postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
+  cors()
 );
 app.use(express.json()); // parse JSON bodies
 
